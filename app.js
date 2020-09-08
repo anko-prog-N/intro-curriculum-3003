ff6f7d04f53ded5a032eaae89d19da8065793253
anko-prog-n <akon8321@yahoo.co.jp>
2020-09-08 20:16:48 +0900

    tribonacci

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict';
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
+const length = process.argv[2] || 0;
 function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
+    if (!memo.has(n)) {
+        memo.set(n, trib(n - 1) + trib(n - 2) + trib(n - 3));
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
+    return memo.get(n);
 }
-const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(trib(i));
 }
+console.log(`Memo Size: ${memo.size}`);
